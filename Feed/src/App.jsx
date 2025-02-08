2e2070aef543e240dfaac5e91ad2dc333ea9ffe6
LeoMedeirosss <lmf@cesar.school>
2025-02-08 18:22:42 -0300

    First CSS Module

--- Feed/src/App.jsx
+++ Feed/src/App.jsx
@@ -1,6 +1,25 @@
+import { Post } from './Post.jsx'
+import { Header } from './components/Header.jsx'
+
+import './styles.css'
+
 function App() {
   return (
-    <h1>Project Feed</h1>
+    <div>
+      <Header/>
+
+      <Post
+      author = "Exemplo Um"
+      content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      Fusce enim orci, laoreet eu dictum at, dignissim eget dolor. Donec imperdiet dolor ac libero dignissim venenatis."
+      />
+
+      <Post
+      author = "Exemplo Dois"
+      content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin congue sagittis imperdiet. 
+      Donec."
+      />
+    </div>
   )
 }
 export default App
