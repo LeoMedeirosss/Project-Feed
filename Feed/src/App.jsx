37686ea07ed1a56f9edbb8274533bc43842af604
LeoMedeirosss <lmf@cesar.school>
2025-02-10 16:34:34 -0300

    Siderbar 1.0

--- Feed/src/App.jsx
+++ Feed/src/App.jsx
@@ -1,24 +1,30 @@
+import './global.css'
+import styles from './App.module.css'
 import { Post } from './Post.jsx'
 import { Header } from './components/Header.jsx'
-
-import './global.css'
+import { Sidebar } from './components/Sidebar.jsx'
 
 function App() {
   return (
     <div>
       <Header/>
 
-      <Post
-      author = "Exemplo Um"
-      content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-      Fusce enim orci, laoreet eu dictum at, dignissim eget dolor. Donec imperdiet dolor ac libero dignissim venenatis."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post
+          author = "Exemplo Um"
+          content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Fusce enim orci, laoreet eu dictum at, dignissim eget dolor. Donec imperdiet dolor ac libero dignissim venenatis."
+        />
 
-      <Post
-      author = "Exemplo Dois"
-      content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin congue sagittis imperdiet. 
-      Donec."
-      />
+        <Post
+          author = "Exemplo Dois"
+          content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin congue sagittis imperdiet. 
+          Donec."
+        />
+        </main>
+      </div>
     </div>
   )
 }
