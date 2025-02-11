e5984c828743fd570fc324532d29ecdf18c32774
LeoMedeirosss <lmf@cesar.school>
2025-02-11 20:36:57 -0300

    App Post

--- Feed/src/App.jsx
+++ Feed/src/App.jsx
@@ -13,17 +13,9 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-        <Post
-          author = "Exemplo Um"
-          content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Fusce enim orci, laoreet eu dictum at, dignissim eget dolor. Donec imperdiet dolor ac libero dignissim venenatis."
-        />
+        <Post/>
 
-        <Post
-          author = "Exemplo Dois"
-          content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin congue sagittis imperdiet. 
-          Donec."
-        />
+        <Post/>
         </main>
       </div>
     </div>
